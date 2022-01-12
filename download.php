f83ac7b210d316148f0c908746ad2b0752105ce0
Water-Boi <91918720+Water-Boi@users.noreply.github.com>
2022-01-11 20:49:03 -0500

    Create download.php

---
+++ download.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="/css.css">
+    <title> Download - Skidproof </title>
+  </head>
+  <body>
+    <center>   <!-- ik this is depreacated but one of us will update it later in css -->
+      <h1>Yay!</h1>
+      <h2>Thank you</h2>
+      <p>Thank you for downloading skidproof</p>
+      <p>Your download should start early but if not click <a href=/releases/latest.js>here</a>
+        <footer>
+          ©Skidproof 2022
+      </footer>
+    </center>
+  </body>
+</html>

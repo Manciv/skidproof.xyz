14f62ad5eda463dd761e04320d3b60b717298b74
Water-Boi <91918720+Water-Boi@users.noreply.github.com>
2022-04-04 01:42:32 -0400

    Update index.php
    actually did something

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
-
+<!DOCTYPE HTML>
+<html>
+<head>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
 
 <link rel="stylesheet" href="/css.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
 
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <h1>Skidproof</h1>
+    <h2>Stop Script-Kitties from stealing your code</h2>
+  </body>
+</html>
